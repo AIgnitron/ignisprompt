4c30f0720b47c62a79d1b90e9ec35b254cd69ba1
Alaa-Aignitron <alaa@aignitron.com>
2026-05-03 23:30:54 -0400

    Remove Cargo target artifacts from git

--- target/debug/ignispromptd.d
+++
@@ -1,1 +0,0 @@
-/Users/alaajallad/Downloads/Aignitron/IgnisPrompt/Code/ignisprompt/target/debug/ignispromptd: /Users/alaajallad/Downloads/Aignitron/IgnisPrompt/Code/ignisprompt/crates/ignispromptd/src/main.rs
